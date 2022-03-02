90d39540816d284aae06c09102e9d1a9969adc94
yanisbourad <77418129+yanisbourad@users.noreply.github.com>
2022-03-02 00:57:55 -0500

    Update backTest.py

--- backTest.py
+++ backTest.py
@@ -1,35 +1,197 @@
+import pandas as pd
+from binance.client import Client
+import ta
+import pandas_ta as pda
+import matplotlib.pyplot as plt
+import numpy as np
+from termcolor import colored
 
-from coin import coin
-import time
-from plyer import notification
+client = Client()
 
-if __name__ == '__main__':
-    btc = coin("BTCUSDT","10 october 2020","1h")
-    btc.initializeDataframeCoin()
-    df = btc.getDataframeCoin()
-    usdt = 1000
-    btc = 0
-    lastIndex = df.first_valid_index()
+klinesT = client.get_historical_klines("EGLDUSDT", Client.KLINE_INTERVAL_1HOUR, "01 january 2017")
 
-    for index, row in df.iterrows():
-        if df["SMA200"][lastIndex] > df["SMA600"][lastIndex] and usdt > 10:
-            btc = usdt / df["close"][index]
-            btc = btc - 0.007 * btc
-            usdt = 0
-            prixAchat = df["close"][-1]
-            print("buy BTC at", df["close"][index], "$ the", index)
+df = pd.DataFrame(klinesT, columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore' ])
+df['close'] = pd.to_numeric(df['close'])
+df['high'] = pd.to_numeric(df['high'])
+df['low'] = pd.to_numeric(df['low'])
+df['open'] = pd.to_numeric(df['open'])
 
-        if df["SMA200"][lastIndex] < df["SMA600"][lastIndex] and btc > 0.0001:
-            usdt = btc * df["close"][index]
-            usdt = usdt - 0.007 * usdt
-            btc = 0
-            prixVente = df["close"][-1]
-            print("sell BTC at", df["close"][index], "$ the", index)
+df = df.set_index(df['timestamp'])
+df.index = pd.to_datetime(df.index, unit='ms')
+del df['timestamp']
+print(df)
 
-        lastIndex = index
+# ----- Functions Definition -------
+def get_chop(high, low, close, window):
+    tr1 = pd.DataFrame(high - low).rename(columns = {0:'tr1'})
+    tr2 = pd.DataFrame(abs(high - close.shift(1))).rename(columns = {0:'tr2'})
+    tr3 = pd.DataFrame(abs(low - close.shift(1))).rename(columns = {0:'tr3'})
+    frames = [tr1, tr2, tr3]
+    tr = pd.concat(frames, axis = 1, join = 'inner').dropna().max(axis = 1)
+    atr = tr.rolling(1).mean()
+    highh = high.rolling(window).max()
+    lowl = low.rolling(window).min()
+    ci = 100 * np.log10((atr.rolling(window).sum()) / (highh - lowl)) / np.log10(window)
+    return ci
 
-    finalResult = usdt + btc * df["close"].iloc[-1]
-    print("final result", finalResult, "USDT")
-    print("buy and hold result", (1000 / df["close"].iloc[0] * df["close"].iloc[-1]), "USDT")
+df.drop(df.columns.difference(['open','high','low','close','volume']), 1, inplace=True)
+
+#Simple Moving Average
+# df['SMA']=ta.trend.sma_indicator(df['close'], window=12)
+
+#Exponential Moving Average
+df['EMA1']=ta.trend.ema_indicator(close=df['close'], window=7)
+df['EMA2']=ta.trend.ema_indicator(close=df['close'], window=30)
+df['EMA3']=ta.trend.ema_indicator(close=df['close'], window=50)
+df['EMA4']=ta.trend.ema_indicator(close=df['close'], window=100)
+df['EMA5']=ta.trend.ema_indicator(close=df['close'], window=121)
+df['EMA6']=ta.trend.ema_indicator(close=df['close'], window=200)
+df['STOCH_RSI'] = ta.momentum.stochrsi(close=df['close'], window=14, smooth1=3, smooth2=3)
+
+dfTest = df.copy()
+# dfTest = df['2021-01-01':]
+dt = None
+dt = pd.DataFrame(columns = ['date','position', 'reason', 'price', 'frais' ,'fiat', 'coins', 'wallet', 'drawBack'])
+
+usdt = 1000
+initalWallet = usdt
+coin = 0
+wallet = 1000
+lastAth = 0
+previousRow = dfTest.iloc[0]
+makerFee = 0.0003
+takerFee = 0.0007
+stopLoss = 0
+takeProfit = 500000
+buyReady = True
+sellReady = True
+
+def buyCondition(row, previousRow):
+  if row['EMA1'] > row['EMA2'] and row['EMA2'] > row['EMA3'] and row['EMA3'] > row['EMA4'] and row['EMA4'] > row['EMA5'] and row['EMA5'] > row['EMA6'] and row['STOCH_RSI']<0.82:
+    return True
+  else:
+    return False
+
+  
+def sellCondition(row, previousRow):
+  if row['EMA6'] > row['EMA1'] and row['STOCH_RSI']>0.2:
+    return True
+  else:
+    return False
 
 
+for index, row in dfTest.iterrows():
+  #Buy market order
+  if buyCondition(row, previousRow) == True and usdt > 0 and buyReady == True:
+    #You can define here at what price you buy
+    buyPrice = row['close']
+
+    #Define the price of you SL and TP or comment it if you don't want a SL or TP
+    # stopLoss = buyPrice - 2 * row['ATR']
+    # takeProfit = buyPrice + 4 * row['ATR']
+
+    coin = usdt / buyPrice
+    fee = takerFee * coin
+    coin = coin - fee
+    usdt = 0
+    wallet = coin * row['close']
+    if wallet > lastAth:
+      lastAth = wallet
+
+    # print("Buy COIN at",buyPrice,'$ the', index)
+    myrow = {'date': index,'position': "Buy", 'reason': 'Buy Market','price': buyPrice,'frais': fee*row['close'],'fiat': usdt,'coins': coin,'wallet': wallet,'drawBack':(wallet-lastAth)/lastAth}
+    dt = dt.append(myrow,ignore_index=True)
+  
+  #Stop Loss
+  elif row['low'] < stopLoss and coin > 0:
+
+    sellPrice = stopLoss
+
+    usdt = coin * sellPrice
+    fee = makerFee * usdt
+    usdt = usdt - fee
+    coin = 0
+    buyReady = False
+    wallet = usdt
+    if wallet > lastAth:
+      lastAth = wallet
+    # print("Sell COIN at Stop Loss",sellPrice,'$ the', index)
+    myrow = {'date': index,'position': "Sell", 'reason': 'Sell Stop Loss', 'price': sellPrice, 'frais': fee, 'fiat': usdt, 'coins': coin, 'wallet': wallet, 'drawBack':(wallet-lastAth)/lastAth}
+    dt = dt.append(myrow,ignore_index=True)
+
+  #Take Profit
+  elif row['high'] > takeProfit and coin > 0:
+
+    sellPrice = takeProfit
+
+    usdt = coin * sellPrice
+    fee = makerFee * usdt
+    usdt = usdt - fee
+    coin = 0
+    buyReady = False
+    wallet = usdt
+    if wallet > lastAth:
+      lastAth = wallet
+    # print("Sell COIN at Take Profit Loss",sellPrice,'$ the', index)
+    myrow = {'date': index,'position': "Sell", 'reason': 'Sell Take Profit', 'price': sellPrice, 'frais': fee, 'fiat': usdt, 'coins': coin, 'wallet': wallet, 'drawBack':(wallet-lastAth)/lastAth}
+    dt = dt.append(myrow,ignore_index=True)     
+
+  # Sell Market
+  elif sellCondition(row, previousRow) == True:
+    buyReady = True
+    if coin > 0 and sellReady == True:
+      sellPrice = row['close']
+      usdt = coin * sellPrice
+      frais = takerFee * usdt
+      usdt = usdt - frais
+      coin = 0
+      wallet = usdt
+      if wallet > lastAth:
+        lastAth = wallet
+      # print("Sell COIN at",sellPrice,'$ the', index)
+      myrow = {'date': index,'position': "Sell", 'reason': 'Sell Market', 'price': sellPrice, 'frais': frais, 'fiat': usdt, 'coins': coin, 'wallet': wallet, 'drawBack':(wallet-lastAth)/lastAth}
+      dt = dt.append(myrow,ignore_index=True)
+  
+  previousRow = row
+
+#///////////////////////////////////////
+print("Period : [" + str(dfTest.index[0]) + "] -> [" +str(dfTest.index[len(dfTest)-1]) + "]")
+dt = dt.set_index(dt['date'])
+dt.index = pd.to_datetime(dt.index)
+dt['resultat'] = dt['wallet'].diff()
+dt['resultat%'] = dt['wallet'].pct_change()*100
+dt.loc[dt['position']=='Buy','resultat'] = None
+dt.loc[dt['position']=='Buy','resultat%'] = None
+
+dt['tradeIs'] = ''
+dt.loc[dt['resultat']>0,'tradeIs'] = 'Good'
+dt.loc[dt['resultat']<=0,'tradeIs'] = 'Bad'
+
+iniClose = dfTest.iloc[0]['close']
+lastClose = dfTest.iloc[len(dfTest)-1]['close']
+holdPorcentage = ((lastClose - iniClose)/iniClose) * 100
+algoPorcentage = ((wallet - initalWallet)/initalWallet) * 100
+vsHoldPorcentage = ((algoPorcentage - holdPorcentage)/holdPorcentage) * 100
+
+print("Starting balance : 1000 $")
+print("Final balance :",round(wallet,2),"$")
+print("Performance vs US Dollar :",round(algoPorcentage,2),"%")
+print("Buy and Hold Performence :",round(holdPorcentage,2),"%")
+print("Performance vs Buy and Hold :",round(vsHoldPorcentage,2),"%")
+print("Number of negative trades : ",dt.groupby('tradeIs')['date'].nunique()['Bad'])
+print("Number of positive trades : ",dt.groupby('tradeIs')['date'].nunique()['Good'])
+print("Average Positive Trades : ",round(dt.loc[dt['tradeIs'] == 'Good', 'resultat%'].sum()/dt.loc[dt['tradeIs'] == 'Good', 'resultat%'].count(),2),"%")
+print("Average Negative Trades : ",round(dt.loc[dt['tradeIs'] == 'Bad', 'resultat%'].sum()/dt.loc[dt['tradeIs'] == 'Bad', 'resultat%'].count(),2),"%")
+idbest = dt.loc[dt['tradeIs'] == 'Good', 'resultat%'].idxmax()
+idworst = dt.loc[dt['tradeIs'] == 'Bad', 'resultat%'].idxmin()
+print("Best trade +"+str(round(dt.loc[dt['tradeIs'] == 'Good', 'resultat%'].max(),2)),"%, the ",dt['date'][idbest])
+print("Worst trade",round(dt.loc[dt['tradeIs'] == 'Bad', 'resultat%'].min(),2),"%, the ",dt['date'][idworst])
+print("Worst drawBack", str(100*round(dt['drawBack'].min(),2)),"%")
+print("Total fee : ",round(dt['frais'].sum(),2),"$")
+reasons = dt['reason'].unique()
+for r in reasons:
+  print(r+" number :",dt.groupby('reason')['date'].nunique()[r])
+
+dt[['wallet','price']].plot(subplots=True, figsize=(20,10))
+print('PLOT')
+plt.show()
